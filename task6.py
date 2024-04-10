8817fb34a1048ef51f8da256d468662e70b89156
Niwa97 <48857497+Niwa97@users.noreply.github.com>
2024-04-10 12:37:58 +0200

    Update task6.py

--- task6.py
+++ task6.py
@@ -8,7 +8,7 @@ class Vector:
         self.z = z
 
     def __repr__(self):
-      return "Vector("+self.x+self.y+self.z+")"
+      return "Vector("+self.x+","+self.y+","+self.z+")"
 
     def __eq__(self, other):
       if(self.x == other.x and self.y == other.y and self.z == other.z):
